2545dc90b3431f7b42cf26d2140e3d8f932b98ec
rashminainwal01 <rashmi.nainwal@kiwitech.com>
2022-07-18 16:06:23 +0530

    crudtask

--- employee/views.py
+++ employee/views.py
@@ -9,53 +9,53 @@ class EmployeeAPIView(viewsets.ModelViewSet):
     queryset = Employee
     serializer_class = EmployeeSerializer
 
-    # def get_serializer_class(self):
+    # def serializer_class(self):
     #     if self.request.method == 'GET':
     #         return EmployeeSerializer
     #     return EmployeeSerializer
 
-    # def create(self, request, *args, **kwargs):
-    #     serializer = self.serializer_class(data=request.data, context=kwargs)
-    #     if serializer.is_valid():
-    #         serializer.save()
-    #
-    #         return Response(serializer.data, status=status.HTTP_201_CREATED)
-    #     else:
-    #         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    def get_queryset(self):
+        if self.request.method == 'GET':
+            queryset = self.queryset.objects.all()
+            return queryset
 
     def create(self, request, *args, **kwargs):
-        serializer = self.get_serializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
+        serializer = self.serializer_class(data=request.data, context=kwargs)
+        if serializer.is_valid():
+            serializer.save()
+
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    # def create(self, request, *args, **kwargs):
+    #     serializer = self.get_serializer(data=request.data)
+    #     serializer.is_valid(raise_exception=True)
+    #     serializer.save()
+    #     return Response(serializer.data, status=status.HTTP_201_CREATED)
 
     def list(self, request, *args, **kwargs):
-        queryset = Employee.objects.all().order_by('-id')
-        serializer = self.get_serializer(queryset, many=True)
-        # serializer = EmployeeSerializer(queryset, many=True)
-        return Response(serializer.data)
-
-    def retrieve(self, request, *args, **kwargs):
-        # serializer = EmployeeSerializer
-        # return Response(serializer.data)
-        instance = self.get_object()
-        serializer = self.get_serializer(instance)
+        queryset = self.get_queryset()
+        serializer = self.serializer_class(queryset, many=True)
         return Response(serializer.data)
 
     def destroy(self, request, *args, **kwargs):
-        # employee = self.get_object()
-        # employee.delete()
-        # return Response(status=status.HTTP_204_NO_CONTENT)
-        instance = self.get_object()
-        instance.delete()
+        employee_id = self.kwargs['pk']
+        queryset = Employee.objects.filter(id=employee_id)
+        queryset.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
 
     def update(self, request, *args, **kwargs):
-        # serializer = EmployeeSerializer
-        # return Response(serializer.data)
-        instance = self.get_object()
-        serializer = self.get_serializer(instance, data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data)
+        employee_id = self.kwargs['pk']
+        queryset = Employee.objects.filter(id=employee_id).first()
+        serializer = self.serializer_class(queryset, data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+    def retrieve(self, request, *args, **kwargs):
+        employee_id = self.kwargs['pk']
+        queryset = Employee.objects.filter(id=employee_id).first()
+        serializer = self.serializer_class(queryset)
+        return Response(serializer.data)
